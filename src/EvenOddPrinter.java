98a4d4daae231b0ed54a2f14de297d8c947dd026
Rishabh Soni <yrishabhsaraf.1112@gmail.com>
2026-07-05 01:44:39 +0530

    Odd-Even via Multi Threading

---
+++ src/EvenOddPrinter.java
@@ -0,0 +1,64 @@
+public class EvenOddPrinter
+{
+	private static int counter = 1;
+	private static final int MAX_NUM = 10;
+	private static final Object lock = new Object();
+
+	public static void main(String[] args)
+	{
+		Thread oddThread = new Thread(() ->
+		{
+			while(counter <= MAX_NUM)
+			{
+				synchronized(lock)
+				{
+					if(counter % 2 != 0)
+					{
+						System.out.println(Thread.currentThread().getName() + ": " + counter);
+						counter++;
+						lock.notify(); // Wake up the even thread
+					}
+					else
+					{
+						try
+						{
+							lock.wait();
+						}
+						catch(InterruptedException e)
+						{
+						}
+					}
+				}
+			}
+		}, "OddThread");
+
+		Thread evenThread = new Thread(() ->
+		{
+			while(counter <= MAX_NUM)
+			{
+				synchronized(lock)
+				{
+					if(counter % 2 == 0)
+					{
+						System.out.println(Thread.currentThread().getName() + ": " + counter);
+						counter++;
+						lock.notify(); // Wake up the odd thread
+					}
+					else
+					{
+						try
+						{
+							lock.wait();
+						}
+						catch(InterruptedException e)
+						{
+						}
+					}
+				}
+			}
+		}, "EvenThread");
+
+		oddThread.start();
+		evenThread.start();
+	}
+}
