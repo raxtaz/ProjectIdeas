8892d189ecf77aedc445c8b60b12c4e938eda31b
Rishabh Soni <yrishabhsaraf.1112@gmail.com>
2026-07-06 01:42:14 +0530

    Updated SimpleHashMapExample

---
+++ src/SimpleHashMapExample.java
@@ -0,0 +1,26 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class SimpleHashMapExample
+{
+	public static void main(String[] args)
+	{
+		LinkedHashMap<String, Integer> fruitInventory = new LinkedHashMap<>();
+
+		fruitInventory.put("Apple", 55);
+		fruitInventory.put("Banana", 40);
+		fruitInventory.put("Pear", 30);
+		fruitInventory.put("Orange", 20);
+		fruitInventory.put("Coconut", 25);
+
+		System.out.println("\nFull Inventory: " + fruitInventory);
+		System.out.println(fruitInventory.get("Pear") + " Pear in inventory.");
+		System.out.println("\n----- Looping through the inventory -----");
+		for(Map.Entry<String, Integer> pooriList : fruitInventory.entrySet())
+		{
+			System.out.println("Fruit name: " + pooriList.getKey() + " & Quantity: " + pooriList.getValue());
+		}
+		fruitInventory.remove("Pear");
+		System.out.println("\nFull Inventory after removing Pear: " + fruitInventory);
+	}
+}
