a17cd627c43e3ef4d8b5a49fe351d1971cadf145
Rishabh Soni <yrishabhsaraf.1112@gmail.com>
2026-07-05 23:59:17 +0530

    LinkedHashMapDemo added

---
+++ src/LinkedHashMapDemo.java
@@ -0,0 +1,17 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class LinkedHashMapDemo
+{
+	public static void main(String[] args)
+	{
+		LinkedHashMap<String, Integer> linkedHashMap = new LinkedHashMap<>(11, 0.8f, true); //double linked list
+		linkedHashMap.put("Orange", 10);
+		linkedHashMap.put("Apple", 20);
+		linkedHashMap.put("Guava", 13);
+		for(Map.Entry<String, Integer> entry : linkedHashMap.entrySet())
+		{
+			System.out.println(entry.getKey() + ": " + entry.getValue());
+		}
+	}
+}
