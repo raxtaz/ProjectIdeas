c7b936601363573ae70d30572b68cb9a0d7280d2
Rishabh Soni <yrishabhsaraf.1112@gmail.com>
2026-07-08 23:11:53 +0530

    Added WeakHashMap

---
+++ src/SimpleWeakHashMap.java
@@ -0,0 +1,27 @@
+import java.util.Map;
+import java.util.WeakHashMap;
+
+public class SimpleWeakHashMap
+{
+	public static void main(String[] args)
+	{
+		//Create map
+		Map<Object, String> map = new WeakHashMap<>();
+
+		//Create an object as key
+		Object key = new Object();
+
+		//Put in map
+		map.put(key, "Hidden Treasure");
+		System.out.println("1. Map before GC: " + map);
+
+		//Destroy only reference to key
+		key = null;
+
+		//Clean JVM memory
+		System.gc();
+
+		//Check map again
+		System.out.println("2. Map after GC: " + map);
+	}
+}
