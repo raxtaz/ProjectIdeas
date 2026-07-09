92ede056752d54d23900839f6fcb73a98bcc9bc2
Rishabh Soni <yrishabhsaraf.1112@gmail.com>
2026-07-10 00:57:08 +0530

    Code cleanup

--- src/ComparatorDemo.java
+++ src/ComparatorDemo.java
@@ -4,37 +4,37 @@ import java.util.List;
 
 class Employee
 {
-int id;
-String name;
-double salary;
+	int id;
+	String name;
+	double salary;
 
-Employee(int id, String name, double salary)
-{
-	this.id = id;
-	this.name = name;
-	this.salary = salary;
-}
+	Employee(int id, String name, double salary)
+	{
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	}
 
-@Override
-public String toString()
-{
-	return id + " " + name + " " + salary;
-}
+	@Override
+	public String toString()
+	{
+		return id + " " + name + " " + salary;
+	}
 }
 
 public class ComparatorDemo
 {
-static void main(String[] args)
-{
-	List<Employee> employees = new ArrayList<>();
+	static void main(String[] args)
+	{
+		List<Employee> employees = new ArrayList<>();
 
-	employees.add(new Employee(101, "Raghav", 50000));
-	employees.add(new Employee(102, "Shivank", 225000));
-	employees.add(new Employee(103, "Anusha", 100000));
-	employees.add(new Employee(104, "Sunaina", 35000));
+		employees.add(new Employee(101, "Raghav", 50000));
+		employees.add(new Employee(102, "Shivank", 225000));
+		employees.add(new Employee(103, "Anusha", 100000));
+		employees.add(new Employee(104, "Sunaina", 35000));
 
-	employees.sort(Comparator.comparingDouble((Employee emp) -> emp.salary).reversed());
+		employees.sort(Comparator.comparingDouble((Employee emp) -> emp.salary).reversed());
 
-	System.out.println(employees);
-}
+		System.out.println(employees);
+	}
 }
