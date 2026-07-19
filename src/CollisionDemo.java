501e37acbdfa9f13a60eb3ad18a2f833fdb590ca
Rishabh Soni <yrishabhsaraf.1112@gmail.com>
2026-07-19 23:14:02 +0530

    Added CollisionDemo

---
+++ src/CollisionDemo.java
@@ -0,0 +1,41 @@
+public class CollisionDemo
+{
+	String[] table;
+
+	public CollisionDemo(int size)
+	{
+		table = new String[size];
+	}
+
+	public int hash(int key)
+	{
+		return key % table.length;
+	}
+
+	public void put(int key, String value)
+	{
+		int index = hash(key);
+
+		System.out.println("Key " + key + " goes to index " + index);
+		table[index] = value;
+	}
+
+	public void display()
+	{
+		System.out.println();
+		for(int i = 0; i < table.length; i++)
+		{
+			System.out.println(i + "->" + table[i]);
+		}
+	}
+
+	public static void main(String[] args)
+	{
+		CollisionDemo ht = new CollisionDemo(10);
+
+		ht.put(11, "Apple");
+		ht.put(21, "Banana");
+
+		ht.display();
+	}
+}
