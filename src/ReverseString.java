94bf803a19c962ad5bed139cc3e42a37da7809ca
Rishabh Soni <yrishabhsaraf.1112@gmail.com>
2026-07-23 15:20:44 +0530

    Code cleanup

--- src/ReverseString.java
+++ src/ReverseString.java
@@ -6,16 +6,19 @@ public class ReverseString
 
 		// 1. Original Brute Force Approach
 		StringBuilder reversed = new StringBuilder();
+
 		for(int i = str.length() - 1; i >= 0; i--)
 		{
 			reversed.append(str.charAt(i));
 		}
+
 		System.out.println("Original: " + str);
 		System.out.println("Reversed (Brute Force): " + reversed);
 
 		// 2. The Algorithmic Optimized Approach (Two Pointers)
 		// This is optimal for interviews. It swaps characters in-place
 		// within an array, cutting the iterations in half (N/2).
+
 		char[] charArray = str.toCharArray();
 		int left = 0;
 		int right = charArray.length - 1;
@@ -37,6 +40,7 @@ public class ReverseString
 		// 3. The Production Optimized Approach (Built-in)
 		// This is the best way to do it in real-world Java development.
 		// It relies on highly optimized, underlying Java source code.
+
 		String reversedBuiltIn = new StringBuilder(str).reverse().toString();
 		System.out.println("Reversed (Built-in): " + reversedBuiltIn);
 	}
